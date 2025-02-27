9a318006989ec47c65d73af82b790b15c6dde9cc
PengGao1027 <37321872+petergaobro@users.noreply.github.com>
2025-02-27 12:39:47 +1100

    create model migration

--- server/djangoProject/api/models.py
+++ server/djangoProject/api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Flower(models.Model):
+    title = models.CharField(max_length=50)
+    release_year = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
